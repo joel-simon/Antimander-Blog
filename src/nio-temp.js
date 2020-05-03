80ed0ae6a653b6863feb1fff1dc343b57f6eb34e
Nio Ono <me@nioono.com>
2020-05-02 19:04:46 -0700

    lots of fixes for nav, scrolling, layout, and visual details

--- src/nio-temp.js
+++ src/nio-temp.js
@@ -11,37 +11,13 @@ observer.observe($nav);
 
 
 // Place sticky nav at correct height
-const $overview = document.querySelector("#overview");
-const $header = document.querySelector("header");
 const $cover = document.querySelector("#cover");
 const $navOl = $nav.querySelector("ol");
+
 function updateNavY() {
-    console.log($navOl.offsetHeight);
     const y = ($cover.offsetTop + $cover.offsetHeight) - ($navOl.offsetHeight);
+    console.log(y);
     $nav.style.setProperty("--overview-offset-top", `${y}px`);
 }
 window.onresize = updateNavY;
-/*
-window.dispatchEvent(new Event('resize'));
-*/
 setTimeout(updateNavY, 100);
-
-// updateNavY();
-/*
-
-
-// Get an element's distance from the top of the page
-var getElemDistance = function ( elem ) {
-    var location = 0;
-    if (elem.offsetParent) {
-        do {
-            location += elem.offsetTop;
-            elem = elem.offsetParent;
-        } while (elem);
-    }
-    return location >= 0 ? location : 0;
-};
-var elem = $overview;
-
-console.log(getElemDistance( elem ))
-*/
