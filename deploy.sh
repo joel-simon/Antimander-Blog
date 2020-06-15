dd8ab0798d18a0dafbdc4455735197b367bfa526
Joel Simon <joelsimon6@gmail.com>
2020-06-15 00:52:33 -0700

    compile typo fixes and remove old build script

--- deploy.sh
+++
@@ -1,3 +0,0 @@
-aws s3 sync --acl public-read --sse --delete build s3://www.antimander.org
-aws s3 sync --acl public-read --sse --delete  --exclude "*" \
-    --include "imgs/*"  --include "icons/*" --include "data/*" --include "fonts/*" . s3://www.antimander.org
